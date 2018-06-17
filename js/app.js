609e844c97fca829710dde5a6b47d9a617b55e45
javfer <javfer79@gmail.com>
2018-06-17 12:41:06 -0500

    colocacion de signos y validacion de telas asi como agregar el signo y punto

--- js/app.js
+++ js/app.js
@@ -1,129 +1,203 @@
-var presionada;
+var id_presionada;
 var enPantalla;
+var paraPantalla;
+var Arreglo=[];
+var validarFinArreglo=0;
 
-document.onkeypress= t_Presionada;
+document.onkeypress= t_PresionadaTeclado;
 document.onkeydown= t_imagenS;
 document.onkeyup= t_imagenG;
 document.onclick=t_PresionadaMouse;
 
 function t_PresionadaMouse(teclas){
   var resive=document.getElementById(teclas.target.id);
-  presionada=resive.id;
-  validarTecla(presionada);
-
+  id_presionada=resive.id;
+  //t_imagenS(id_presionada);
   enPantalla=document.querySelector("#calculadoraFondo #display").innerText;
-  muestraDisplay(enPantalla, presionada);
+  validarTecla(id_presionada);
+
+  //t_imagenG(id_presionada);
+  document.querySelector("#calculadoraFondo #display").innerHTML=paraPantalla;
 }
 
-function t_Presionada(tecla){
+function t_PresionadaTeclado(tecla){
   //var presionada=tecla.which || tecla.keyCode;
-presionada=tecla.key
-validarTecla(presionada);
-
+id_presionada=tecla.key
 enPantalla=document.querySelector("#calculadoraFondo #display").innerText;
-muestraDisplay(enPantalla, presionada);
+validarTecla(id_presionada);
+
+document.querySelector("#calculadoraFondo #display").innerHTML=paraPantalla;
 }
 
-function t_imagenG(presionada) {
-  document.getElementById(presionada).style="height:4.6vw;";//width:5.7vw;
+function t_imagenG() {
+  document.getElementById(id_presionada).style="height:4.6vw;";//width:5.7vw;
+  //return;
+  }
+
+function t_imagenS() {
+  document.getElementById(id_presionada).style="height:4.5vw;";//width:5.63vw;
+  //return;
+  }
+
+function paraDisplay(){
+  if  (validarFinArreglo==1){
+  if (Arreglo.length>0){
+
+    var nume=Arreglo.length;
+    var carater=Arreglo[nume-1];
+      if(carater=="+"){
+        document.querySelector("#calculadoraFondo #display").innerHTML="0";
+        enPantalla="";
+      }
+    }
+    validarFinArreglo=0;
+  }
+
+  if (id_presionada=="on") {
+    paraPantalla="0";
+    document.querySelector(".nota-input")
+    .innerHTML="";
+    return;
+}else if (enPantalla=="0"){
+enPantalla="";
+}else if(enPantalla.length==10){
+  var valor=Number(enPantalla);
+  if (valor<=0){
+    return;
+  }
+}else if (enPantalla.length==9) {
+return;
 }
 
-function t_imagenS(presionada) {
-  document.getElementById(tecla.key).style="height:4.5vw;";//width:5.63vw;
+  paraPantalla=enPantalla+id_presionada;
+  return;
 }
+ function validarTecla(id_presionada) {
+   switch (id_presionada) {
 
-function muestraDisplay(enPantalla, presionada){
-  document.querySelector("#calculadoraFondo #display")
-  .innerHTML=(presionada);
-}
- function validarTecla(presionada) {
-   switch (presionada) {
     case "0":
-     alert(presionada);
+     paraDisplay();
     return;
      break;
 
     case "1":
-      alert(presionada);
+      paraDisplay();
     return;
        break;
 
     case "2":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
 
     case "3":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
     case "4":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
     case "5":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
     case "6":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
     case "7":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
 
     case "8":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
 
     case "9":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
 
     case "on":
-      alert(presionada);
+      paraDisplay();
     return;
     break;
 
     case "sign":
-      alert(presionada);
+      cambiaSigno();
     return;
     break;
 
     case "raiz":
-      alert(presionada);
+    var agregaSigno="raiz"
+    agregarOperacion(agregaSigno);
     return;
     break;
     case "dividido":
-      alert(presionada);
+    var agregaSigno="/"
+    agregarOperacion(agregaSigno);
     return;
     break;
     case "por":
-      alert(presionada);
+    var agregaSigno="*"
+    agregarOperacion(agregaSigno);
     return;
     break;
     case "menos":
-      alert(presionada);
+    var agregaSigno="-"
+    agregarOperacion(agregaSigno);
     return;
     break;
     case "punto":
-      alert(presionada);
+      colocarPunto()
+
     return;
     break;
     case "igual":
-      alert(presionada);
+      alert(id_presionada);
     return;
     break;
     case "mas":
-      alert(presionada);
+      var agregaSigno="+"
+      agregarOperacion(agregaSigno);
     return;
     break;
-     default:
-  alert("esto es un default");
    }
 
  }
+
+function cambiaSigno() {
+  var valor=Number(enPantalla);
+  valor=valor*-1;
+  paraPantalla=valor.toString();
+  return;
+
+}
+
+function agregarOperacion(agregaSigno) {
+  Arreglo.push(enPantalla);
+  Arreglo.push(agregaSigno);
+  var paraUnirArre="";
+  for (var i = 0; i < Arreglo.length; i++) {
+    var valorArreglo=Arreglo[i];
+    paraUnirArre=paraUnirArre+valorArreglo;
+}
+document.querySelector(".nota-input")
+.innerHTML=paraUnirArre;
+validarFinArreglo=1;
+  return;
+}
+
+function colocarPunto() {
+  for (var i=0; i<enPantalla.length; i++){
+    var caracter = enPantalla.charAt(i);
+    if (caracter=="."){
+      return;
+    }
+        }
+  paraPantalla=enPantalla+".";
+  return;
+}
