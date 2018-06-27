f4b144af3b1e94b4cf1f83b1a2a159ce9fe6827d
javfer <javfer79@gmail.com>
2018-06-26 19:49:44 -0500

    falta el punto e poo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var valor1;
+
 var valor2;
-var operando;
+
 
 function Calculadora() {
   var resultado=document.getElementById("display");
@@ -23,114 +23,251 @@ function Calculadora() {
   var suma=document.getElementById('mas');
   var punto=document.getElementById('punto');
   var igual=document.getElementById('igual');
-
+  var arreglo=[];
+  var operador;
+  var operando;
 
 uno.onclick = function(e){
      revisaCeroInicial();
      resultado.textContent = resultado.textContent  + "1";
+     longitud();
  }
+
 dos.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "2";
+      longitud();
   }
+
 tres.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "3";
+      longitud();
   }
+
 cuatro.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "4";
+      longitud();
 }
+
 cinco.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "5";
+      longitud();
 }
+
 seis.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "6";
+      longitud();
 }
+
 siete.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "7";
+      longitud();
 }
+
 ocho.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "8";
+      longitud();
 }
+
 nueve.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "9";
+      longitud();
 }
+
 cero.onclick = function(e){
       revisaCeroInicial();
       resultado.textContent = resultado.textContent  + "0";
+      longitud();
 }
+
+punto.onclick = function(e){
+  colocarPunto();
+}
+
 signo.onclick = function(e){
   resultado.textContent = (resultado.textContent * -1);
 }
+
 on.onclick = function(e){
-    limpiar();
+  borrar();
+  //resultado.textContent = "0";
 }
-on.dblclick = function(e){
-    borrar();
-}
+
 suma.onclick = function(e) {
-    valor1=resultado.textContent;
-    operando=suma;
-    limpiar();
+    operando=resultado.textContent;
+    operador="+";
+    agregar();
 }
+
 resta.onclick = function(e) {
-    valor1=resultado.textContent;
-    operando=resta;
-    limpiar();
+    operando=resultado.textContent;
+    operador="-";
+    agregar();
 }
+
 multiplica.onclick = function(e) {
-    valor1=resultado.textContent;
-    operando=multiplica;
-    limpiar();
+    operando=resultado.textContent;
+    operador="*";
+    agregar();
 }
+
 division.onclick = function(e) {
-    valor1=resultado.textContent;
-    operando=division;
-    limpiar();
+    operando=resultado.textContent;
+    operador="/";
+    agregar();
 }
+
 raiz.onclick = function(e) {
-    valor1=resultado.textContent;
-    limpiar();
+    arreglo=[];
+    operando=resultado.textContent;
+    operador="√";
+    agregar();
+    resuelve();
 }
+
 igual.onclick = function(e){
-    valor2=resultado.textContent;
-    resuelve(operando);
+    if (arreglo.length==1){
+      arreglo.push(operador);
+      arreglo.push(operando);
+    }else{
+      operando=resultado.textContent;
+      agregar();
+    }
+    resuelve();
 
 }
-function resuelve(operando) {
-  switch (operando) {
-   case suma:
-  resultado.textContent=Number(valor1) + Number(valor2);
-  Number(valor1) = Number(valor2);
-    break;
-    case resta:
-   resultado.textContent=Number(valor1) - Number(valor2);
-     break;
-     case multiplica:
-    resultado.textContent=Number(valor1) * Number(valor2);
-      break;
-      case divide:
-     resultado.textContent=Number(valor1) + Number(valor2);
-       break;
+
+function resuelve() {
+  operador;
+  operando;
+var total=0;
+
+for (i=0; i<arreglo.length-1;i++){//realiza raiz
+
+  var valor1=Number(arreglo[i]);
+//  var logitudValorRaiz=arreglo[i].length;
+//  var valorRaiz=arreglo[i].slice(1,logitudValorRaiz);
+  var valor2=Number(valor1);
+
+      if (arreglo[i+1]=="√" && Number(valor2)){//calcula raiz
+
+        total=Math.sqrt(valor2);
+        var res =total.toString();
+        arreglo[i]=res.slice(0,9);
+        arreglo.splice(i+1,1);
+        i=0;
+      }
+  }//fin for raiz
+      for (i=0; i<arreglo.length;i++){//ejecuta y elimina * /
+
+        var valor1=Number(arreglo[i-1]);
+        var valor2=Number(arreglo[i+1]);
+
+      if (arreglo[i]=="*" && Number(valor2)){
+
+          total=valor1*valor2;
+          i=corrijeArreglo(total,i);
+
+      }else if (arreglo[i]=="/" && Number(valor2)){
+
+          total=valor1/valor2;
+          i=corrijeArreglo(total,i);
+        }
+      }//fin for*/
+
+for(i=0;i<arreglo.length; i++){
+
+var valor1=Number(arreglo[i-1]);
+var valor2=Number(arreglo[i+1]);
+
+      if (arreglo[i]=="-" && Number(valor2)){
+
+          total=valor1-valor2;
+          i=corrijeArreglo(total,i);
+
+      }else if (arreglo[i]=="+" && Number(valor2)){
+
+          total=valor1+valor2;
+          i=corrijeArreglo(total,i);
+
+      }
+}//fin for +-
+
+document.querySelector(".nota-input")
+.innerHTML=arreglo[0];
+resultado.textContent=arreglo[0];
+
+return;
 }
-}
-function limpiar() {
+
+function agregar() {
+  arreglo.push(operando);
+  arreglo.push(operador);
   resultado.textContent="0";
+  //operador="";
+  //operando="";
 }
+
 function revisaCeroInicial() {
-  if (resultado.textContent<=0){
-    resultado.textContent="";
+  i = colocarPunto();
+
+    if (resultado.textContent == 0){
+        if (i!=true){
+      resultado.textContent = "";
+    }
   }
 }
+
 function borrar() {
-  valor1=0;
-  valor2=0;
+  operador="";
+  operando="";
+  arreglo=[];
   resultado.textContent="0";
+}
+
+function corrijeArreglo(total, i) {
+      var res =total.toString();
+      arreglo[i]=res.slice(0,9);
+      arreglo.splice(i+1,1);
+      arreglo.splice(i-1,1);
+      i=0;
+      var valor1=arreglo.length;
+        if ((arreglo[valor1-1]=="+")||(arreglo[valor1-1]=="-")||
+        (arreglo[valor1-1]=="*")||(arreglo[valor1-1]=="/")||(arreglo[valor1-1]=="√")){
+          arreglo.splice(valor1-1,1);
+        }
+      return(i);
+}
+
+function longitud() {
+  i = colocarPunto();
+  var longitud=resultado.textContent;
+
+  if (i==true){
+      resultado.textContent=longitud.substring(0, 9);
+  }else{
+      resultado.textContent=longitud.substring(0, 8);
+  }
+
+
+}
+
+function colocarPunto() {
+  var longitud=resultado.textContent;
+  for (var i=0; i< longitud.length; i++){
+    var caracter = longitud.charAt(i);
+    if (caracter=="."){
+      return true;
+    }
+        }
+  resultado.textContent = resultado.textContent + ".";
+  return false;
 }
 }
